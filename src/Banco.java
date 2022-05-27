36eaee231a8dfc1364d45ee630bc857357fdea6b
Daiana Alves <daianaalves93@outlook.com.br>
2022-05-27 20:09:01 -0300

    adicionando atributos e metodos na clase

--- src/Banco.java
+++ src/Banco.java
@@ -1,4 +1,5 @@
 
 public class Banco {
+	public String nome;
 
 }
--- src/ContaPoupança.java
+++ src/ContaPoupança.java
@@ -1,4 +1,14 @@
 
 public class ContaPoupança {
+	public int agencia;
+	public int numero;
+	public double saldo;
+	
+	
+	public void sacar() {}
+	
+	public void depositar() {}
+	
+	public void transferir() {}
 
 }
