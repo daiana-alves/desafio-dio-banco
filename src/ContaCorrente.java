23435a871770847675ab68be51faaffd12220833
Daiana Alves <daianaalves93@outlook.com.br>
2022-05-30 20:37:49 -0300

    implementando heranca interface

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,26 +1,4 @@
 
-public class ContaCorrente {
-	private int agencia;
-	private int numero;
-	private double saldo;
-	
-	public int getAgencia() {
-		return agencia;
-	}
-
-	public int getNumero() {
-		return numero;
-	}
-
-	public double getSaldo() {
-		return saldo;
-	}
-
-	public void sacar() {}
-	
-	public void depositar() {}
-	
-	public void transferie() {}
-	
+public class ContaCorrente extends Conta{
 
 }
